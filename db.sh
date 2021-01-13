bfb8866773876f5440f455ebede40ae224a7a5bc
Olivier Filangi <olivier.filangi@gmail.com>
2021-01-13 14:55:44 +0100

    update with FlavonoidsCombinatoire

--- db.sh
+++ db.sh
@@ -12,14 +12,15 @@ function db() {
 
   # here all databse to manage
   foodb
+  FlavonoidsCombinatoire
   newdb
 }
 
 function foodb() {
-   
-  nameg=$(pushd $basedir;ls foodb*;popd)
-  nameg=$(basename $nameg)
+  echo " -- FooDB --"
 
+  nameg=$(basename "`ls $basedir/foo*`" ".ttl")
+ 
   echo " -- Loading $nameg"
   ISQLV_CMD="ld_dir('/usr/local/virtuoso-opensource/share/virtuoso/vad/dumps/', 'foodb*.ttl', 'http://p2m2.metabohub.org/"$nameg"'); rdf_loader_run();"
 
@@ -29,6 +30,21 @@ function foodb() {
 
   echo " -- Foodb Loaded."
 }
+
+function FlavonoidsCombinatoire() {
+  echo " -- FlavonoidsCombinatoire --"
+  nameg="FlavonoidsCombinatoire"
+
+  echo " -- Loading $nameg"
+  ISQLV_CMD="ld_dir('/usr/local/virtuoso-opensource/share/virtuoso/vad/dumps/', 'flavonoids_combinatoire.ttl', 'http://p2m2.metabohub.org/"$nameg"'); rdf_loader_run();"
+
+  docker exec \
+          ${CONTAINER_NAME} \
+          isql-v 1111 dba "${PASSWORD}" exec="${ISQLV_CMD}"
+
+  echo " -- FlavonoidsCombinatoire Loaded."
+}
+
 
 function newdb() {
    echo " -- new db --"
